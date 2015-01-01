785d201c9bb81c03360ed09315ddaf86b469cc0d
Easwar Aiyer <easwarismyname@gmail.com>
2015-01-01 13:05:38 +0530

    separated config parameters

---
+++ app/main.php
@@ -0,0 +1,16 @@
+<?php
+
+$app = "offermama";
+
+$paths = array(
+    "config" => "/$app/config/",
+    "common" => "/$app/common",
+    "migrations" => "/$app/migrations",
+    "app" => "/$app/app",
+);
+
+
+//include main config file
+require_once $paths["config"]."database.php";
+
+?>
