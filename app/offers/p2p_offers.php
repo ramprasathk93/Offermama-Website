394ebd11f1a617f2a7f0cccef0a90b92011296ff
aiyer <aiyer@localhost>
2015-01-18 11:26:57 +0530

    desing of p2p offers, changed user_ad for posting MRP and SP, Changed comments to reviews. Changed display of comments.

--- app/offers/p2p_offers.php
+++ app/offers/p2p_offers.php
@@ -2,33 +2,55 @@
 require_once "../../config/database.php";
 $year=$_POST['year'];
 if($year==0){
+    $str='No results in general category, try others';
     foreach($conn->query("select * from postuser where target='all'") as $row){
-		echo '<div class="panel poffer" id="'.$row['postid'].'">'.$row['content'].'</div>';
-	
-    }    
+        
+        foreach($conn->query('select * from user_info where u_id="'.$row['userid'].'"')as $k){
+            include 'poffer-template.php';
+            
+        }
+        $str='';
+    }
+    echo $str;
 }
 else if($year==1){
+    $str='No results in first year category, try others';
     foreach($conn->query("select * from postuser where target='firstyr'") as $row){
-		echo '<div class="panel poffer" id="'.$row['postid'].'">'.$row['content'].'</div>';
-	
+        foreach($conn->query('select * from user_info where u_id="'.$row['userid'].'"')as $k){
+            include 'poffer-template.php';
+        }
+        $str='';
     }
+    echo $str;
 }
 else if($year==2){
+    $str='No results in second year category, try others';
     foreach($conn->query("select * from postuser where target='secondyr'") as $row){
-		echo '<div class="panel poffer" id="'.$row['postid'].'">'.$row['content'].'</div>';
-	
+        foreach($conn->query('select * from user_info where u_id="'.$row['userid'].'"')as $k){
+            include 'poffer-template.php';
+        }
+        $str='';
     }   
+    echo $str;
 }
 else if($year==3){
+    $str='No results in third year category, try others';
     foreach($conn->query("select * from postuser where target='thirdyr'") as $row){
-		echo '<div class="panel poffer" id="'.$row['postid'].'">'.$row['content'].'</div>';
-	
+        foreach($conn->query('select * from user_info where u_id="'.$row['userid'].'"')as $k){
+            include 'poffer-template.php';
+        }
+        $str='';
     }   
+    echo $str;
 }
 else if($year==4){
+    $str='No results in fourth year category, try others';
     foreach($conn->query("select * from postuser where target='fourthyr'") as $row){
-		echo '<div class="panel poffer" id="'.$row['postid'].'">'.$row['content'].'</div>';
-	
+        foreach($conn->query('select * from user_info where u_id="'.$row['userid'].'"')as $k){
+            include 'poffer-template.php';
+        }
+        $str='';
     }   
+    echo $str;
 }
 ?>
