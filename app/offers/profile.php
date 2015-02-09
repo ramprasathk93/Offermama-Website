7d7cfed68eb49aa0f880c9c9e1c0d3b6b3642d01
aiyer <aiyer@localhost>
2015-02-10 00:28:21 +0530

    Beta Version 1.1

--- app/offers/profile.php
+++ app/offers/profile.php
@@ -19,6 +19,32 @@ else {
 <script src="../../common/js/jquery.min.js"></script>
 <script src="functions.js"></script>
 <script src="../../common/js/foundation/foundation.js"></script>
+    <style>
+    .offer{
+        background-color:#efefef;
+        font-family:"Open Sans";
+        font-size:100%;
+        color:#000;
+        margin-top:0;
+        border-radius:2px;
+        box-shadow:0px 2px 5px 0px rgba(0,0,0,0.50);
+          opacity:0.9;
+    }
+        .prof dd{
+        color:#FFF;
+            box-shadow:0px 2px 5px 0px rgba(0,0,0,0.50);
+            opacity:1;
+        }
+        .p2p{
+        margin-top:0;
+            background-color:#efefef;
+            opacity:0.95;
+            height:50%;
+            border-radius:2px;
+           box-shadow:0px 2px 5px 0px rgba(0,0,0,0.70);
+        }
+        
+    </style>
 <script>
 $(document).ready(function(){
     click_posts("<?php echo $uid;?>");
@@ -35,15 +61,16 @@ function click_delete(pid){
 }
 </script>
 </head>
-<body>
+<body style="background-color:#efefef;background-image:url(images/suit1.jpg);background-size:cover">
+   
 <?php if (login_check($mysqli) == true) : ?>
 <div class"contain-to-grid fixed">
-<nav class="top-bar tb" data-topbar>
+<nav class="top-bar profile" data-topbar>
 <ul class="title-area">
-<li class="name">
+<li class="name" >
 <h1>
 <a href="home.php">
-Offermama
+Offermama<sup style="font-size:80%">beta</sup>
 </a>
 </h1>
 </li>
@@ -51,13 +78,12 @@ Offermama
 </ul>
 <section class="top-bar-section">
  
-<ul class="left">
-<li class="divider"></li>
-<li><a href='user_ad.php'>Sell your stuff</a></li>
-<li class="divider"></li>
-</ul>
+
  
 <ul class="right">
+    <li class="divider"></li>
+<li><a href='user_ad.php'>Sell your stuff</a></li>
+<li class="divider"></li>
 <li class="divider"></li>
 <li><a href="profile.php">Profile</a></li>
 <li class="divider"></li>
@@ -69,14 +95,15 @@ Offermama
 </nav>
 </div>
 <br/>
-<div class="row">
+<div class="row prof_offer"> 
+  
     <div class="small-12 large-2 small-uncentered columns small-12 maincategory">
-         <dl class="tabs pill vertical">
+         <dl class="tabs pill vertical prof">
             <dd><a id="posts" onclick=click_posts("<?php echo $uid;?>")>Posts</a></dd>
             <dd><a id="grooming" onclick=click_settings("<?php echo $uid; ?>")>Settings</a></dd>
         </dl>
     </div>
-    <div class="small-6 large-6 columns">
+    <div class="small-6 large-6 columns view">
         <div class="wall1">
         </div>
     </div>
@@ -84,7 +111,9 @@ Offermama
         <div class="p2p">
         </div>
     </div>
+  
 </div>
+   
 <?php else : ?>
         <p>
             <span class="error">You are not authorized to access this page.</span> Please <a href="login.php">login</a>.
