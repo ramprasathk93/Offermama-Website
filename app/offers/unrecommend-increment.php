e9fe8d4f3b2f6f2e4776debbf11f9eb19a253ccb
aiyer <aiyer@localhost>
2015-02-10 22:31:02 +0530

    bug fix(like, recommend, unrecommend, google analytics)

--- app/offers/unrecommend-increment.php
+++ app/offers/unrecommend-increment.php
@@ -1,13 +1,19 @@
 <?php
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
+sec_session_start();
 $bid=$_POST['bid'];
+$uid=$_POST['uid'];
 require_once "../../config/database.php";
 foreach($conn->query('select * from business_info where b_id="'.$bid.'"') as $k){
     $likes=$k['unrecommend'];
 }
+if(!strcmp($_SESSION['email'],$uid)){
 $likes=$likes+1;
 $stmt = $conn->prepare("update business_info set unrecommend=:l where b_id=:bid");
 $stmt->bindParam(':l',$likes);
 $stmt->bindParam(':bid',$bid);
 $stmt->execute();
+}
 echo $likes;
 ?>
