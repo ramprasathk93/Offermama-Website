cc254b5921eac6a8d68955f00123469a12abc08d
aiyer <aiyer@localhost>
2015-02-11 00:33:44 +0530

    like module(bug) unfixed

--- app/offers/like-decrement.php
+++ app/offers/like-decrement.php
@@ -5,15 +5,22 @@ sec_session_start();
 $pid=$_POST['pid'];
 $uid=$_POST['uid'];
 require_once "../../config/database.php";
+$already_exist=0;
+
 if(!strcmp($_SESSION['email'],$uid)){
 foreach($conn->query('select * from post_b where post_id="'.$pid.'"') as $k){
     $likes=$k['likes_count'];
 }
+foreach($conn->query('select * from likes where post_id="'.$pid.'" and u_id="'.$uid.'"') as $k){
+        $already_exist=1;
+}
+if($already_exist == 1){
 $likes=$likes-1;
 $stmt = $conn->prepare("update post_b set likes_count=:l where post_id=:pid");
 $stmt->bindParam(':l',$likes);
 $stmt->bindParam(':pid',$pid);
 $stmt->execute();
+}
 echo $likes.' likes';
 }
 else{
