ca1a139051ea0f7715de3c2b8019c8cea5de1fa2
aiyer <aiyer@localhost>
2015-02-19 22:48:05 +0530

    Bug fix( like module )

--- app/offers/unlike.php
+++ app/offers/unlike.php
@@ -4,20 +4,33 @@ include_once 'includes/functions.php';
 sec_session_start();
 $uid=$_POST['uid'];
 $pid=$_POST['pid'];
-$already_exist=0;
 require_once "../../config/database.php";
+$exists=0;
+
 if(!strcmp($_SESSION['email'],$uid)){
+    foreach($conn->query('select * from post_b where post_id="'.$pid.'"') as $k){
+    $likes=$k['likes_count'];
+}
     foreach($conn->query('select * from likes where post_id="'.$pid.'" and u_id="'.$uid.'"') as $k){
-        $already_exist=1;
-    }
-    if($already_exist == 1){
+        $exists=1;
+    }   
+    if($exists==1){
     $stmt = $conn->prepare("delete from likes where u_id= :userid and post_id=:postid");
     $stmt->bindParam(':userid',$uid);
     $stmt->bindParam(':postid',$pid);
     $stmt->execute();
-    }
+    $likes=$likes-1;
+    $stmt = $conn->prepare("update post_b set likes_count=:l where post_id=:pid");
+    $stmt->bindParam(':l',$likes);
+    $stmt->bindParam(':pid',$pid);
+    $stmt->execute();
     echo '<a onclick=click_like("'.$pid.'","'.$uid.'")>Like</a>';
+    }
+    else{
+        echo '<a onclick=click_like("'.$pid.'","'.$uid.'")>Like</a>';
+    }
 }
+
 else{
     echo '<a onclick=click_unlike("'.$pid.'","'.$uid.'")>Unlike</a>';
 }
